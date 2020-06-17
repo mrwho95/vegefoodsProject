76ee33579d1ec3c51158a9f3bfd88eb86d2d9e34
mrwho95 <dnkh903@gmail.com>
2020-06-17 15:33:44 +0800

    customer able to send message and store message to db

--- app/Http/Controllers/User/ContactController.php
+++ app/Http/Controllers/User/ContactController.php
@@ -4,10 +4,33 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\customermessage;
+use Validator;
 
 class ContactController extends Controller
 {
     public function index(){
     	return view('user.contact');
+    }
+
+    public function storeMessage(Request $request, customermessage $customerMessage){
+     	$validator = Validator::make($request->all(), [
+	        'name' => 'required|min:3',
+	        'email' => 'required|min:3',
+	        'subject' => 'required|min:3',
+	        'message' => 'required|min:3',
+	    ]);
+
+	    if ($validator->fails()) {
+	        return back()->with('warning', $validator->messages()->all()[0])->withInput();
+	    }
+
+	    $customerMessage->name = $request->name;
+	    $customerMessage->email = $request->email;
+	    $customerMessage->subject = $request->subject;
+	    $customerMessage->message = $request->message;
+	    $customerMessage->save();
+
+    	return redirect()->route('contact')->with('success', 'Message sent! We will contact you as soon as possible.');
     }
 }
--- resources/views/user/contact.blade.php
+++ resources/views/user/contact.blade.php
@@ -13,7 +13,7 @@
 
 <section class="ftco-section contact-section bg-light">
 	<div class="container">
-		<div class="row d-flex mb-5 contact-info">
+		<!-- <div class="row d-flex mb-5 contact-info">
 			<div class="w-100"></div>
 			<div class="col-md-3 d-flex">
 				<div class="info bg-white p-4">
@@ -35,24 +35,40 @@
 					<p><span>Website</span> <a href="#">yoursite.com</a></p>
 				</div>
 			</div>
-		</div>
+		</div> -->
 		<div class="row block-9">
 			<div class="col-md-6 order-md-last d-flex">
-				<form action="#" class="bg-white p-5 contact-form">
+				<form action="{{route('storeMessage')}}" class="bg-white p-5 contact-form">
+					@if(session('success'))
+						<div class="alert alert-success alert-dismissible fade show" role="alert">
+							{{session('success')}}
+							<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+					@endif
+					@if(session('warning'))
+						<div class="alert alert-warning alert-dismissible fade show" role="alert">
+							{{session('warning')}}
+							<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+					@endif
 					<div class="form-group">
-						<input type="text" class="form-control" placeholder="Your Name">
+						<input type="text" class="form-control" name="name" placeholder="Your Name" required>
 					</div>
 					<div class="form-group">
-						<input type="text" class="form-control" placeholder="Your Email">
+						<input type="text" class="form-control" name="email" placeholder="Your Email" required>
 					</div>
 					<div class="form-group">
-						<input type="text" class="form-control" placeholder="Subject">
+						<input type="text" class="form-control" name="subject" placeholder="Subject" required>
 					</div>
 					<div class="form-group">
-						<textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
+						<textarea name="message" cols="30" rows="7" class="form-control" placeholder="Message" required></textarea>
 					</div>
 					<div class="form-group">
-						<input type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
+						<button type="submit" class="btn btn-primary py-3 px-5">Send Message</button>
 					</div>
 				</form>
 				
