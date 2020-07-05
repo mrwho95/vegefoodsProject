d474180eb5e73e6a5d3ac61fd4148b885680a447
mrwho95 <dnkh903@gmail.com>
2020-07-05 17:34:37 +0800

    update shop and order controller to latest version

--- app/Http/Controllers/User/OrderController.php
+++ app/Http/Controllers/User/OrderController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\order;
+use App\orderdetails;
+use Auth;
 
 class OrderController extends Controller
 {
@@ -12,7 +15,44 @@ class OrderController extends Controller
 		return view('user.order');
 	}
 
-    public function order(){
-        return redirect()->route('order')->with('success', 'Order Successfully!');
+    public function orderProcess(Request $request, order $order, orderdetails $orderDetails){
+        //json_decode is convert string to php object
+        // return $userDetails;
+        // foreach ($user_details as $key => $data) {
+            // return $data['name'];
+        // $address = join(',' $userDetails);
+        // return $address;
+        $userDetails = json_decode($request['user_details'], true);
+        $address = $userDetails[4]['value'].$userDetails[5]['value'].", ".$userDetails[7]['value']." ".$userDetails[6]['value'].", ".$userDetails[8]['value']." ".$userDetails[9]['value'];
+
+        $order->user_id = Auth::id();
+        $order->fullname = $userDetails[1]['value'];
+        $order->phonenumber = $userDetails[2]['value'];
+        $order->address = $address;
+        $order->discount = null;
+        $order->delivery = $request['deliveryFee'];
+        $order->totalprice = $request['overallCost'];
+        $order->save();
+
+        //no use save() to store orderDetails because it just update the same row. Either create new when don't have id or update row when got id.
+        //save() method is used both for saving new model, and updating existing one. here you are creating new model or find existing one, setting its properties one by one and finally saves in database.
+
+        // Create method you are passing an array, setting properties in model and persists in the database in one shot.
+        $product = json_decode($request['product'],true);
+        foreach ($product as $key => $data) {
+             $orderDetails->create([
+                'order_id' => $order->id, 
+                'product_id' => $data['id'],
+                'productName' => $data['name'],
+                'productPrice' => $data['price'],
+                'wishQuantity' => $data['inCart']
+            ]);
+        }
+
+        return response()->json(['success' => 'Data is successfully updated', 'data'=>"sucesss"]);    
+    }
+
+    public function promo(Request $request){
+    	return redirect()->route('cart')->with('success', 'Enjoy the promotion!');
     }
 }
--- app/Http/Controllers/User/ShopController.php
+++ app/Http/Controllers/User/ShopController.php
@@ -5,30 +5,93 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Product;
+use App\address;
+use App\user;
+use App\order;
+use App\deliveryplace;
+use Auth, Validator;
 
 class ShopController extends Controller
 {
     public function index(){
         $arr['allProduct'] = product::paginate(8);
+        // $arr['allProductData'] = product::all();
     	return view('user.shop', $arr);
     }
 
     public function cart(){
-    	return view('user.cart');
+        if (Auth::check()) {
+            $arr['address'] = address::where([['user_id', Auth::id()], ['defaultaddress', '1']])->first();
+            $arr['defaultAddress'] = address::where([['user_id', Auth::id()], ['defaultaddress', '1']])->first();
+            $address = json_decode(json_encode($arr['address']), true);
+            $arr['deliveryfee'] = deliveryplace::where('state', $address['state'])->value('price');
+        }else{
+            $arr = '';
+        }
+    	return view('user.cart', $arr);
+    }
+
+    public function checkDeliveryFee(Request $request){
+        if (Auth::check()) {
+            $arr['defaultAddress'] = address::where([['user_id', Auth::id()], ['defaultaddress', '1']])->first();
+        }
+        $validator = Validator::make($request->all(), [
+            'city' => 'required|min:3',
+            'state' => 'required|min:3',
+            'postcode' => 'required|min:3',
+            'country' => 'required|min:3',
+        ]);
+
+        if ($validator->fails()) {
+            return back()->with('warning', $validator->messages()->all()[0])->withInput();
+        }
+
+        $city = $request->city;
+        $state = $request->state;
+        $postcode = $request->postcode;
+        $country = $request->country;
+
+        $arr['deliveryfee'] = deliveryplace::where([['city', $city], ['state', $state], ['postcode', $postcode], ['country', $country]])->value('price');
+
+        if (empty($arr['deliveryfee'])) {
+            return redirect()->route('cart')->with('warning', "delivery fee of that place not found.");
+        }
+
+        $arr['address'] = array(
+            'city' => $city,
+            'state' => $state,
+            'postcode' => $postcode,
+            'country' => $country
+        );
+
+        // return redirect()->route('cart')->with('deliveryfee', $deliveryfee);
+        return view('user.cart', $arr)->with('success', 'Estimate successful!');
     }
 
     public function checkout(){
-    	return view('user.checkout');
+        // $order = order::with('orderdetails')->get();
+        //  return response()->json(['data' => $order]);
+
+        if (Auth::check()) {
+            $arr['user'] = user::find(Auth::id());
+        }else{
+            $arr = '';
+        }
+    	return view('user.checkout', $arr);
     }
 
     public function showProduct($parameter){
         $category = ['vegetable', 'fruit', 'fruit juice', 'meat', 'bakery', 'seafood'];
         if (in_array($parameter, $category)) {
             $arr['allProduct'] = product::where('category', $parameter)->paginate(8);
+            $arr['allProductData'] = product::all();
             return view('user.shop', $arr); 
         }
         else{
             $arr['singleProduct'] = product::find($parameter);
+            $category = product::where('id', $parameter)->value('category');
+            // $arr['relatedProduct'] = product::where('category', $category)->get()->random(4);
+            $arr['relatedProduct'] = product::where('id', '!=', $parameter)->get()->random(4);
             return view('user.productSingle', $arr); 
         }
     }
