87cd90eaecd28a791bb72666d2661b3b554fbfc7
mrwho95 <dnkh903@gmail.com>
2020-07-06 03:42:16 +0800

    update stock, update orderunique id and status, user able to view their purchase items

--- app/Http/Controllers/User/OrderController.php
+++ app/Http/Controllers/User/OrderController.php
@@ -6,13 +6,20 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\order;
 use App\orderdetails;
+use App\product;
 use Auth;
 
 class OrderController extends Controller
 {
     //
 	public function index(){
-		return view('user.order');
+        if (Auth::check()) {
+            $arr['order'] = order::where('user_id', Auth::id())->with('orderdetails')->get();
+            $arr['product'] = product::all();
+        }else{
+            $arr = '';
+        }
+		return view('user.order', $arr);
 	}
 
     public function orderProcess(Request $request, order $order, orderdetails $orderDetails){
@@ -26,12 +33,14 @@ class OrderController extends Controller
         $address = $userDetails[4]['value'].$userDetails[5]['value'].", ".$userDetails[7]['value']." ".$userDetails[6]['value'].", ".$userDetails[8]['value']." ".$userDetails[9]['value'];
 
         $order->user_id = Auth::id();
+        $order->orderunique_id = "#".uniqid();
         $order->fullname = $userDetails[1]['value'];
         $order->phonenumber = $userDetails[2]['value'];
         $order->address = $address;
         $order->discount = null;
         $order->delivery = $request['deliveryFee'];
         $order->totalprice = $request['overallCost'];
+        $order->status = "Success";
         $order->save();
 
         //no use save() to store orderDetails because it just update the same row. Either create new when don't have id or update row when got id.
@@ -40,13 +49,23 @@ class OrderController extends Controller
         // Create method you are passing an array, setting properties in model and persists in the database in one shot.
         $product = json_decode($request['product'],true);
         foreach ($product as $key => $data) {
-             $orderDetails->create([
+            $orderDetails->create([
                 'order_id' => $order->id, 
                 'product_id' => $data['id'],
                 'productName' => $data['name'],
                 'productPrice' => $data['price'],
                 'wishQuantity' => $data['inCart']
             ]);
+
+            $productStock = product::where(['id' => $data['id']])->value('quantity');
+            if ($productStock >= $data['inCart'] ) {
+                $productStock = bcsub($productStock, $data['inCart']);
+                $update = product::where('id',  $data['id'])->update(['quantity' => $productStock]);
+            }else{
+                //error
+                 return response()->json(['error' => 'Failed to order', 'data'=>"product stock not enough."]);
+            }
+            
         }
 
         return response()->json(['success' => 'Data is successfully updated', 'data'=>"sucesss"]);    
