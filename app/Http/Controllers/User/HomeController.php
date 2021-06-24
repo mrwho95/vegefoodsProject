478e0884f74a27d7f8f51ba4d096b9713b8249dc
mrwho95 <dnkh903@gmail.com>
2021-06-25 01:42:12 +0800

    try laravel log and code revise

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -8,10 +8,22 @@ use App\product;
 use Carbon\Carbon;
 use App\order;
 use Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index(){
+        // $message = "Hellow World";
+//         Log::emergency($message);
+// Log::alert($message);
+// Log::critical($message);
+// Log::error($message);
+// Log::warning($message);
+// Log::notice($message);
+// Log::info($message);
+// Log::debug($message);
+
     	$arr['allProduct'] = product::all()->random(8);
 
     	// $today = Carbon::now()->format('Y-m-d');
@@ -19,23 +31,40 @@ class HomeController extends Controller
         // info($today); store info at storage / logs/ laravel.log
 
             // return $day;
-    	$orderData = order::where('user_id', Auth::id())->with('orderdetails')->get();
-        foreach ($orderData as $key => $elements) {
-            $createDate = $elements['created_at'];
-            $day = $createDate->addDays(0)->format('Y-m-d');
-            $day2 = $createDate->addDays(1)->format('Y-m-d');
-            $day3 = $createDate->addDays(2)->format('Y-m-d');
-            $day4 = $createDate->addDays(3)->format('Y-m-d');
-            $day5 = $createDate->addDays(4)->format('Y-m-d');
-            if($day2 == $today){
-                order::where('id', $elements['id'])->update(['status' => "Prepare items"]);
-            }elseif($day3 == $today){
-                order::where('id', $elements['id'])->update(['status' => "Prepare for delivery"]);
-            }elseif($day4 == $today){
-                order::where('id', $elements['id'])->update(['status' => "Out to delivery"]);
-            }elseif($day5 <= $today){
-                order::where('id', $elements['id'])->update(['status' => "Completed"]);
+    	// $orderData = order::where('user_id', Auth::id())->with('orderdetails')->get();
+     //    foreach($orderData as $d) {
+     //        echo gettype($d['created_at']);
+     //        $day[] = $d['created_at']->addDays(0)->format('Y-m-d');
+     //    }
+     //    print_r($day);
+     //    return;
+        
+        $day = [];
+        $orderData = '';
+
+        $orders = json_decode(DB::table('orders')->select(['id','created_at'])->get(),true);
+        foreach($orders as $v) {
+            for ($i=0; $i < 6; $i++) { 
+                $day[] = date('Y-m-d',strtotime("+$i day", strtotime($v['created_at'])));
+                // $day[] = $v['created_at']->addDays($i)->format('Y-m-d');
             }
+            switch ($today) {
+                case $day[0]:
+                case $day[1]:
+                    $orderData = ['status' => "Prepare items"];
+                    break;
+                case $day[2]:
+                    $orderData = ['status' => "Prepare for delivery"];
+                    break;
+                case $day[3]:
+                    $orderData = ['status' => "Out to delivery"];
+                    break;
+                default:
+                    $orderData = ['status' => "Completed"];
+                    break;
+            }
+            order::where('id', $v['id'])->update($order);
+
         }
 
     	return view('user.home', $arr);
