d90ce7797220220cf9c2c8f103e5e35039a66e69
mrwho95 <dnkh903@gmail.com>
2021-08-19 05:22:13 +0800

    order logic revise

--- app/Http/Controllers/User/OrderController.php
+++ app/Http/Controllers/User/OrderController.php
@@ -14,58 +14,75 @@ class OrderController extends Controller
 {
     //
 	public function index(){
+        $arr = [];
         if (Auth::check()) {
             $arr['order'] = order::where('user_id', Auth::id())->with('orderdetails')->orderBy('created_at', 'DESC')->get();
             $arr['product'] = product::all();
-        }else{
-            $arr = '';
         }
 		return view('user.order', $arr);
 	}
 
-    public function orderProcess(Request $request, order $order, orderdetails $orderDetails){
+    public function orderProcess(Request $request, order $order, orderdetails $orderDetails) {
         //json_decode is convert string to php object
-        // return $userDetails;
-        // foreach ($user_details as $key => $data) {
-            // return $data['name'];
-        // $address = join(',' $userDetails);
-        // return $address;
-        $userDetails = json_decode($request['user_details'], true);
-        $address = $userDetails[4]['value'].$userDetails[5]['value'].", ".$userDetails[7]['value']." ".$userDetails[6]['value'].", ".$userDetails[8]['value']." ".$userDetails[9]['value'];
-
-        $order->user_id = Auth::id();
-        $order->orderunique_id = "#".uniqid();
-        $order->fullname = $userDetails[1]['value'];
-        $order->phonenumber = $userDetails[2]['value'];
-        $order->address = $address;
-        $order->discount = null;
-        $order->delivery = $request['deliveryFee'];
-        $order->totalprice = $request['overallCost'];
-        $order->status = "Success";
-        $order->save();
 
         //no use save() to store orderDetails because it just update the same row. Either create new when don't have id or update row when got id.
         //save() method is used both for saving new model, and updating existing one. here you are creating new model or find existing one, setting its properties one by one and finally saves in database.
 
         // Create method you are passing an array, setting properties in model and persists in the database in one shot.
-        $product = json_decode($request['product'],true);
-        foreach ($product as $v) {
-            $orderDetails->create([
-                'order_id' => $order->id, 
-                'product_id' => $v['id'],
-                'productName' => $v['name'],
-                'productPrice' => $v['price'],
-                'wishQuantity' => $v['inCart']
-            ]);
+        $products = json_decode($request['product'],true);
+        $productsIds = array_column($products, 'id');
+        // return count($products);
+        $productsStock = product::findMany($productsIds)->pluck('quantity')->toArray();
+        $productsSell = product::findMany($productsIds)->pluck('sell')->toArray();
+        $tmp = [];
+        $tmp2 = [];
+        $i = 0;
+        foreach ($products as $k => $p) {
+            if ($p['inCart'] > $productsStock[$i]) return ['msg'=>$p['name']." insufficient stock"];
+            $tmp =  [
+                'product_id' => $p['id'],
+                'productName' => $p['name'],
+                'productPrice' => $p['price'],
+                'wishQuantity' => $p['inCart'],
+                'created_at' => $orderDetails->freshTimestamp(),
+                'updated_at' => $orderDetails->freshTimestamp()
+            ];
+            $data[] = $tmp;
 
-            product::where(['id' => $v['id']])->update(['sell' => $v['inCart']]);
+            $remainStock = bcsub($productsStock[$i], $p['inCart']);
+            $totalSell = bcadd($productsSell[$i],$p['inCart']);
+            $tmp2[$p['id']]['remainStock'] = $remainStock;
+            $tmp2[$p['id']]['totalSell'] = $totalSell;
 
-            $productStock = product::where(['id' => $v['id']])->value('quantity');
-            if ($productStock < $v['inCart']) return response()->json(['error' => 'Failed to order', 'data'=>"product stock not enough."]);
+            if ($i == count($products)-1) {
+                $userDetails = json_decode($request['user_details'], true);
+                $address = $userDetails[4]['value'].$userDetails[5]['value'].", ".$userDetails[7]['value']." ".$userDetails[6]['value'].", ".$userDetails[8]['value']." ".$userDetails[9]['value'];
 
-            $productStock = bcsub($productStock, $v['inCart']);
-            $update = product::where('id',  $v['id'])->update(['quantity' => $productStock]);
-            
+                $order->user_id = Auth::id();
+                $order->orderunique_id = "#".uniqid();
+                $order->fullname = $userDetails[1]['value'];
+                $order->phonenumber = $userDetails[2]['value'];
+                $order->address = $address;
+                $order->discount = null;
+                $order->delivery = $request['deliveryFee'];
+                $order->totalprice = $request['overallCost'];
+                $order->status = "Success";
+                $order->save();
+
+                foreach($data as $k => $d) {
+                    $data[$k]['order_id'] = $order->id;
+                }
+                orderdetails::insert($data);
+                // using insert method does not insert created_at and updated_at data to db, need to manually insert both
+
+                foreach($tmp2 as $k => $t) {
+                    product::where('id', $k)->update([
+                        'quantity' => $t['remainStock'],
+                        'sell' => $t['totalSell']
+                    ]);
+                }
+            }
+            $i++;
         }
 
         //dispatch job email
@@ -74,7 +91,7 @@ class OrderController extends Controller
         SendMailJob::dispatch($message, $email)->delay(now()->addSeconds(3));;
         // return 'mail sent successfully';
 
-        return response()->json(['success' => 'Data is successfully updated', 'data'=>"sucesss"]);    
+        return "Order Successful";  
     }
 
     public function promo(Request $request){
